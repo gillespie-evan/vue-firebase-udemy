c9b453d72813acc2f1e754178976d9ed02d07abd
Evan Gillespie <gillespie.evan@gmail.com>
2019-03-17 20:18:21 -0400

    another part

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -1,6 +1,13 @@
 new Vue({
     el: '#app',
     data:{
-        title: 'Learning Vue'
+        title: 'Learning Vue',
+        name: 'Evan',
+        url: 'https://www.youtube.com'
+    },
+    methods: {
+        greet(time){
+            return `Hello and good ${time}, ${this.name}, `
+        }
     }
 })
