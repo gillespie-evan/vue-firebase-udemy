76f71397d7e44380ac74fb504aa25e8872dc750e
Evan Gillespie <evan.gillespie@leanix.net>
2019-03-24 19:48:25 -0400

    Adding section 03

---
+++ s03-vue-cli/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+  <Navbar />
+  <AllFriends />
+  <OnlineFriends />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+
+export default {
+  name: 'app',
+  components:{
+    Navbar,
+    OnlineFriends,
+    AllFriends
+  },
+  data () {
+    return {
+      title: 'First Vue app, wee!',
+          friends: [
+                { name: 'Billy', online: true },
+                { name: 'Bob', online: false },
+                { name: 'John', online: true },
+                { name: 'Mario', online: false },
+            ]
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
