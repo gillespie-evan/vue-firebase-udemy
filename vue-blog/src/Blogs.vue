b5b0f883364fe1077cdb614bc0f65da5c083cbf3
Evan Gillespie <gillespie.evan@gmail.com>
2019-03-31 16:04:07 -0400

    Blogs.vue in PS

---
+++ vue-blog/src/Blogs.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="blogs">
+        <h2>Blogs</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Blogs',
+    data {
+        return{
+
+        }
+    },
+    methods: {
+
+    },
+    created(){
+        
+    }
+}
+</script>
+
