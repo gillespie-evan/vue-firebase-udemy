25c2977b2de5a31c0277aad1f890d6b571f77f14
Evan Gillespie <gillespie.evan@gmail.com>
2019-04-06 16:49:58 -0400

    Finished #24

--- vue-blog/src/Blogs.vue
+++ vue-blog/src/Blogs.vue
@@ -1,22 +1,33 @@
 <template>
     <div class="blogs">
         <h2>Blogs</h2>
+        <div v-for="post in posts" :key="post.id">
+            <h3>{{ post.title }}</h3>
+            <p>{{ post.body }}</p>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'Blogs',
-    data {
+    data() {
         return{
-
+            posts: []
         }
     },
     methods: {
 
     },
     created(){
-        
+        axios.get('https://jsonplaceholder.typicode.com/postss/')
+        .then(response => {
+            this.posts = response.data
+        }).catch(err => {
+            console.log(err)
+        })
     }
 }
 </script>
